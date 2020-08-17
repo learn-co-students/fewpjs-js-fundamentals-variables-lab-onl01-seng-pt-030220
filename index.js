9097257389d8f07f0800e927873cbb88786c3841
SincerelyBrittany <sincerelybr@gmail.com>
2020-08-17 15:32:03 -0400

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 const companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Chelsea';
 var companyCeo = 'Susan Smith';
+
+// Use var... never.
+// Use let... when you know the value of a variable will change. For example, a counter variable that starts at 0 and is subsequently incremented to 1, 2, 3, and so on. In the lessons on looping and iteration in JavaScript, let will have its moment in the spotlight.
+// Use const... for every other variable.
